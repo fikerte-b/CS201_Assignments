3322596fd5820bc35bbec09a5c94fb654cc4b254
gfifi <fikertebenti@gmail.com>
2021-10-16 20:37:07 -0500

    Lab12 oct16 updated

--- src/lab12/Lab12_3.java
+++ src/lab12/Lab12_3.java
@@ -17,14 +17,4 @@ public class Lab12_3 {
 
     }
 }
-//    public static void main(String[] args) {
-//        double[] array = {19,30,50,2};
-//        System.out.println(sumArray(array, 1));
-//
-//    }
-//    public static double sumArray(double[] arr, int index){
-//        double sum = arr[index];
-//        if(index == arr.length -1)
-//            return arr[index];
-//        else {
-//            return sum + sumArray(arr, index + 1);
+
